3678f739193ea8c981da0e226ab9867a54be209c
bonsk5852 <bonsk5852@HRH-IC0027333.wrdsb.ca>
2017-09-19 09:50:43 -0400

    Reverse the binary number in Q5 and Q6 Change numbers greater than 10 to Letters

--- src/recursion/A2Q6.java
+++ src/recursion/A2Q6.java
@@ -13,7 +13,28 @@ public class A2Q6 {
     /**
      * @param args the command line arguments
      */
+    public int convert(int n, int b) {
+        // Create a variable for the binary number
+        int remainder = 0;
+        // If the number = 1 end the loop
+        if (n == 1) {
+            remainder = n;
+            System.out.print(remainder);
+            return remainder;
+        } else {
+            // Find the remainder of the number and output it. Then divide the number by b
+            remainder = n % b;
+            n = n / b;
+            System.out.print(remainder);
+            convert(n, b);
+            return remainder;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Test the method
+        A2Q6 test = new A2Q6();
+        test.convert(1000, 8);
+        System.out.println("");
     }
 }
