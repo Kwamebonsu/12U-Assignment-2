4c524439f18ba5b850915908a1390ecd88ec71fc
bonsk5852 <bonsk5852@HRH-IC0027333.wrdsb.ca>
2017-09-21 10:14:34 -0400

    Assignment 1-4 and 7 complete 5 needs to be reversed 6 needs to be reversed and add letters

--- src/recursion/A2Q6.java
+++ src/recursion/A2Q6.java
@@ -26,7 +26,21 @@ public class A2Q6 {
             remainder = n % b;
             // Divide the number by b
             n = n / b;
-            System.out.print(remainder);
+            if (remainder < 10) {
+                System.out.print(remainder);
+            } else if (remainder == 10) {
+                System.out.println("A");
+            } else if (remainder == 11) {
+                System.out.println("B");
+            } else if (remainder == 12) {
+                System.out.println("C");
+            } else if (remainder == 13) {
+                System.out.println("D");
+            } else if (remainder == 14) {
+                System.out.println("E");
+            } else if (remainder == 15) {
+                System.out.println("F");
+            }
             // Call on the method again
             convert(n, b);
             return remainder;
@@ -36,7 +50,7 @@ public class A2Q6 {
     public static void main(String[] args) {
         // Test the method
         A2Q6 test = new A2Q6();
-        test.convert(1000, 8);
+        test.convert(1000, 16);
         System.out.println("");
     }
 }
